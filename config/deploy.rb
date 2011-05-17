c84cc0c52003c18281e706257c1ded24243e720f
Gianni Chiappetta <gianni@runlevel6.org>
2011-05-16 20:51:09 -0400

    updated: Capistrano settings.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,10 @@
 set :application, "ot-crew"
-set :repository,  "git@github.com:gf3/runlevel6.org.git"
+set :repository,  "git@github.com:gf3/ot-crew.com.git"
 set :deploy_to,   "/data/www/ot-crew.com"
 set :scm,         :git
 set :user,        :gianni
+
+set :use_sudo,    false
 
 role :web, "192.168.10.10"                          # Your HTTP server, Apache/etc
 role :app, "192.168.10.10"                          # This may be the same as your `Web` server
