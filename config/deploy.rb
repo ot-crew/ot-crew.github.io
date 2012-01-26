aed0239b7a74b4db6cade14c8168eb5d8e92c63d
Gianni Chiappetta <gianni@runlevel6.org>
2012-01-25 22:05:31 -0500

    update: Repo location

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,15 @@
 set :application, "ot-crew"
-set :repository,  "git@github.com:gf3/ot-crew.com.git"
+set :repository,  "git@github.com:jquery-ot/ot-crew.com.git"
 set :deploy_to,   "/data/www/ot-crew.com"
 set :scm,         :git
 set :user,        :gianni
 
 set :use_sudo,    false
 
-role :web, "192.168.10.10"                          # Your HTTP server, Apache/etc
-role :app, "192.168.10.10"                          # This may be the same as your `Web` server
+role :web, "192.168.10.10"
+role :app, "192.168.10.10"
 
-after :deploy, "deploy:npm"
+# after :deploy, "deploy:npm"
 
 namespace :deploy do
   task :restart, roles: :app, except: { :no_release => true } do
